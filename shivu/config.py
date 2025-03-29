58a859edb40808d096c5e9ad659503a6d1ba942b
narutoloversasuke <dubbexmadara@gmail.com>
2025-03-29 12:53:37 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,8 +5,8 @@ class Config(object):
     OWNER_ID = "7885908019"
     sudo_users = "7885908019", "7640076990", "7756901810" 
     GROUP_ID = -1002640379822
-    TOKEN = "8136650512:AAFC8c8225hPO_q2AFKtZtADMjCRryF1ZJU"  # Updated Token
-    mongo_url = "mongodb+srv://walkrock:supermanxxxx@sunny.vu6keb7.mongodb.net/?retryWrites=true&w=majority&appName=sunny"
+    TOKEN = "7656541596:AAEkC5jxXlArTehwTS9lmm61Xqi5cLpuPrk"  # Updated Token
+    mongo_url = "mongodb+srv://naruhina:harshitkichut@cluster0.03pl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = "https://files.catbox.moe/0nb1p7.jpg" 
     SUPPORT_CHAT = "blade_x_support"
     UPDATE_CHAT = "blade_x_community"
