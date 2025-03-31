63a17fe059f1352ddb825256dc34a24f5d3ca4fa
narutoloversasuke <dubbexmadara@gmail.com>
2025-03-31 15:04:07 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -28,6 +28,7 @@ message_counts = {}
 # Proper indentation for the 'for' loop
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("shivu.modules." + module_name)
+
 last_user = {}
 warned_users = {}
 
